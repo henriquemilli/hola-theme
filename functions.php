085bd6563ad57768cd969b41edd0a673a31e2d58
henriquemilli <henriquemillipro@gmail.com>
2021-10-13 11:15:59 +0200

    permit webp

--- functions.php
+++ functions.php
@@ -171,12 +171,14 @@ function hola_remove_eicons() {
 
 
 /**
- * Permit SVG files upload
+ * Permit SVG and webp files upload
  */
 function add_file_types_to_uploads ( $file_types ) {
 	$new_filetypes = array();
 	$new_filetypes['svg'] = 'image/svg';
+	$new_filetypes['webp'] = 'image/webp';
 	
+
 	$file_types = array_merge( $file_types, $new_filetypes );
 
 	return $file_types;
